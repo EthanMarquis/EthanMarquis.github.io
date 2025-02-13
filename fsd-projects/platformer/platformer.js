a8a674ff55f1a5f6aa0cb126f9b35786ac516a89
EthanMarquis <nolasoul@gmail.com>
2025-02-13 23:46:21 +0000

    Finalized platformer game

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,21 +27,25 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+    toggleGrid();
 
 
     // TODO 2 - Create Platforms
-
-
-
-
+createPlatform(300, 650, 100, 25, "purple")
+createPlatform(500, 550, 100, 25, "purple")
+createPlatform(300, 425, 100, 25, "purple")
+createPlatform(100, 300, 100, 25, "purple")
+createPlatform(300, 200, 300, 25, "purple")
+createPlatform(975, 550, 100, 25, "purple")
+createPlatform(100, 0, 25, 300, "purple")
     // TODO 3 - Create Collectables
-
-
+createCollectable("database", 1250, 325);
+createCollectable("steve", 150, 250);
+createCollectable("grace", 550, 150);
 
     
     // TODO 4 - Create Cannons
-
+createCannon("bottom", 800, 1500)
 
     
     
